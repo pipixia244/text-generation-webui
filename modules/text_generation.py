d3ee224addaf499d2161871345c8198276595fac
SakuraUmi <yukinon244@gmail.com>
2023-11-01 23:56:46 +0800

    Update text_generation.py

--- modules/text_generation.py
+++ modules/text_generation.py
@@ -9,7 +9,10 @@ import traceback
 import numpy as np
 import torch
 import transformers
-from transformers import LogitsProcessorList, is_torch_xpu_available
+from transformers import LogitsProcessorList, GenerationConfig
+
+def is_torch_xpu_available():
+    return False
 
 import modules.shared as shared
 from modules.callbacks import (
@@ -23,6 +26,15 @@ from modules.html_generator import generate_4chan_html, generate_basic_html
 from modules.logging_colors import logger
 from modules.models import clear_torch_cache, local_rank
 
+generation_config_default = GenerationConfig(
+    temperature=0.1,
+    top_p=0.3,
+    top_k=40,
+    repetition_penalty=1,
+    max_new_token=1024,
+    do_sample=True,
+    num_beams=1
+)
 
 def generate_reply(*args, **kwargs):
     shared.generation_lock.acquire()
@@ -43,7 +55,9 @@ def generate_reply_token(*args, **kwargs):
     finally:
         shared.generation_lock.release()
 
-def _generate_reply(question, state, stopping_strings=None, is_chat=False, escape_html=False, is_return_token_cnt=False):
+def _generate_reply(question, state, stopping_strings=None, is_chat=False, escape_html=False, is_return_token_cnt=False, generation_config=generation_config_default):
+
+    state_tmp = state
 
     # Find the appropriate generation function
     generate_func = apply_extensions('custom_generate_reply')
@@ -66,9 +80,9 @@ def _generate_reply(question, state, stopping_strings=None, is_chat=False, escap
 
     # Find the stopping strings
     all_stop_strings = []
-    for st in (stopping_strings, ast.literal_eval(f"[{state['custom_stopping_strings']}]")):
-        if type(st) is list and len(st) > 0:
-            all_stop_strings += st
+    # for st in (stopping_strings, ast.literal_eval(f"[{state['custom_stopping_strings']}]")):
+    #     if type(st) is list and len(st) > 0:
+    #         all_stop_strings += st
 
     if shared.args.verbose:
         print(f'\n\n{question}\n--------------------\n')
@@ -85,8 +99,9 @@ def _generate_reply(question, state, stopping_strings=None, is_chat=False, escap
         state['stream'] = True
 
     # Generate
+    state['max_tokens_second'] = 0
     if is_return_token_cnt:
-        for reply, token_cnt in generate_func(question, original_question, seed, state, stopping_strings, is_chat=is_chat, is_return_token_cnt=is_return_token_cnt):
+        for reply, token_cnt in generate_func(question, original_question, seed, state_tmp, stopping_strings, is_chat=is_chat, is_return_token_cnt=is_return_token_cnt):
             if escape_html:
                 reply = html.escape(reply)
 
@@ -259,6 +274,7 @@ def fix_galactica(s):
 
 
 def get_reply_from_output_ids(output_ids, input_ids, original_question, state, is_chat=False, is_return_token_cnt=False):
+    state['skip_special_tokens'] = True
     if shared.is_seq2seq:
         new_tokens = len(output_ids)
         reply = decode(output_ids, state['skip_special_tokens'])
@@ -318,38 +334,53 @@ def apply_stopping_strings(reply, all_stop_strings):
 
 
 def generate_reply_HF(question, original_question, seed, state, stopping_strings=None, is_chat=False, is_return_token_cnt=False):
+    
+    question_tmp = question
+    state_tmp = state
+    state['stream'] = False
+    generation_config = GenerationConfig.from_dict(state)
+    # generation_config = GenerationConfig(
+    #     temperature=state['temperature'],
+    #     top_p = state['top_p'],
+    #     top_k = state['top_k'],
+    #     max_new_tokens = state['max_new_tokens'],
+    #     do_sample = state['do_sample'],
+    #     # repetition_penalty = state['repetition_penalty'],
+    #     frequency_penalty = 1.1,
+    #     num_beams = state['num_beams'],
+    #     penalty_alpha = state['penalty_alpha']
+    # )
+    
     generate_params = {}
-    for k in ['max_new_tokens', 'do_sample', 'temperature', 'top_p', 'typical_p', 'repetition_penalty', 'presence_penalty', 'frequency_penalty', 'repetition_penalty_range', 'encoder_repetition_penalty', 'top_k', 'min_length', 'no_repeat_ngram_size', 'num_beams', 'penalty_alpha', 'length_penalty', 'early_stopping', 'tfs', 'top_a', 'mirostat_mode', 'mirostat_tau', 'mirostat_eta', 'guidance_scale']:
-        generate_params[k] = state[k]
+    # for k in ['max_new_tokens', 'do_sample', 'temperature', 'top_p', 'typical_p', 'repetition_penalty', 'presence_penalty', 'frequency_penalty', 'repetition_penalty_range', 'encoder_repetition_penalty', 'top_k', 'min_length', 'no_repeat_ngram_size', 'num_beams', 'penalty_alpha', 'length_penalty', 'early_stopping', 'tfs', 'top_a', 'mirostat_mode', 'mirostat_tau', 'mirostat_eta', 'guidance_scale']:
+    #     generate_params[k] = state[k]
 
-    if state['negative_prompt'] != '':
-        generate_params['negative_prompt_ids'] = encode(state['negative_prompt'])
+    # if state['negative_prompt'] != '':
+    #     generate_params['negative_prompt_ids'] = encode(state['negative_prompt'])
 
-    for k in ['epsilon_cutoff', 'eta_cutoff']:
-        if state[k] > 0:
-            generate_params[k] = state[k] * 1e-4
+    # for k in ['epsilon_cutoff', 'eta_cutoff']:
+    #     if state[k] > 0:
+    #         generate_params[k] = state[k] * 1e-4
 
-    if state['ban_eos_token']:
-        generate_params['suppress_tokens'] = [shared.tokenizer.eos_token_id]
+    # if state['ban_eos_token']:
+    #     generate_params['suppress_tokens'] = [shared.tokenizer.eos_token_id]
 
-    if state['custom_token_bans']:
-        to_ban = [int(x) for x in state['custom_token_bans'].split(',')]
-        if len(to_ban) > 0:
-            if generate_params.get('suppress_tokens', None):
-                generate_params['suppress_tokens'] += to_ban
-            else:
-                generate_params['suppress_tokens'] = to_ban
+    # if state['custom_token_bans']:
+    #     to_ban = [int(x) for x in state['custom_token_bans'].split(',')]
+    #     if len(to_ban) > 0:
+    #         if generate_params.get('suppress_tokens', None):
+    #             generate_params['suppress_tokens'] += to_ban
+    #         else:
+    #             generate_params['suppress_tokens'] = to_ban
 
-    generate_params.update({'use_cache': not shared.args.no_cache})
-    if shared.args.deepspeed:
-        generate_params.update({'synced_gpus': True})
+    # generate_params.update({'use_cache': not shared.args.no_cache})
+    # if shared.args.deepspeed:
+    #     generate_params.update({'synced_gpus': True})
 
     # Encode the input
-    input_ids = encode(question, add_bos_token=state['add_bos_token'], truncation_length=get_max_prompt_length(state))
+    input_ids = encode(question)
     output = input_ids[0]
     cuda = not any((shared.args.cpu, shared.args.deepspeed))
-    if state['auto_max_new_tokens']:
-        generate_params['max_new_tokens'] = state['truncation_length'] - input_ids.shape[-1]
 
     # Add the encoded tokens to generate_params
     question, input_ids, inputs_embeds = apply_extensions('tokenizer', state, question, input_ids, None)
@@ -360,17 +391,17 @@ def generate_reply_HF(question, original_question, seed, state, stopping_strings
 
     # Stopping criteria / eos token
     eos_token_ids = [shared.tokenizer.eos_token_id] if shared.tokenizer.eos_token_id is not None else []
-    generate_params['eos_token_id'] = eos_token_ids
-    generate_params['stopping_criteria'] = transformers.StoppingCriteriaList()
-    generate_params['stopping_criteria'].append(_StopEverythingStoppingCriteria())
+    # generate_params['eos_token_id'] = eos_token_ids
+    # generate_params['stopping_criteria'] = transformers.StoppingCriteriaList()
+    # generate_params['stopping_criteria'].append(_StopEverythingStoppingCriteria())
 
-    processor = state.get('logits_processor', LogitsProcessorList([]))
-    # In case a processor is passed by itself.
-    if not isinstance(processor, LogitsProcessorList):
-        processor = LogitsProcessorList([processor])
-    processor.append(GrammarLogitsProcessor(state['grammar_string']))
-    apply_extensions('logits_processor', processor, input_ids)
-    generate_params['logits_processor'] = processor
+    # processor = state.get('logits_processor', LogitsProcessorList([]))
+    # # In case a processor is passed by itself.
+    # if not isinstance(processor, LogitsProcessorList):
+    #     processor = LogitsProcessorList([processor])
+    # processor.append(GrammarLogitsProcessor(state['grammar_string']))
+    # apply_extensions('logits_processor', processor, input_ids)
+    # generate_params['logits_processor'] = processor
 
     t0 = time.time()
     try:
@@ -383,11 +414,15 @@ def generate_reply_HF(question, original_question, seed, state, stopping_strings
         # Generate the entire reply at once.
         if not state['stream']:
             with torch.no_grad():
-                output = shared.model.generate(**generate_params)[0]
+                print(question_tmp)
+                print(generation_config)
+                output = shared.model.generate(**shared.tokenizer(question_tmp, return_tensors="pt").to(shared.model.device), generation_config=generation_config)[0]
+                print(output)
+                # output = shared.model.generate(**generate_params)[0]
                 if cuda:
                     output = output.cuda()
 
-            yield get_reply_from_output_ids(output, input_ids, original_question, state, is_chat=is_chat, is_return_token_cnt=is_return_token_cnt)
+            yield get_reply_from_output_ids(output, input_ids, original_question, state_tmp, is_chat=is_chat, is_return_token_cnt=is_return_token_cnt)
 
         # Stream the reply 1 token at a time.
         # This is based on the trick of using 'stopping_criteria' to create an iterator.
